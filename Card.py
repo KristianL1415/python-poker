c225bb8eb95ebec804b1e80bdf3b4c48dfdc1626
Kristian Lien <klien@myriadmobile.com>
2015-01-02 10:18:51 -0600

    Added card classes.

---
+++ Card.py
@@ -0,0 +1,7 @@
+# Card.py
+# Stores suit and value for each card
+
+class Card():
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
---
+++ CardDeck.py
@@ -0,0 +1,15 @@
+# CardDeck.py
+# Manages the deck of 52 cards for the poker game
+
+import Card
+
+class CardDeck():
+    # Standard 52 card deck initializer
+    def __init__(self):
+        self.cards = []
+        suits = ["heart", "diamond", "spade", "club"]
+        values = ["2", "3", "4", "5", "6", "7","8", "9", "10", "J", "Q", "K", "A"]
+
+        for i in range(0, 4):
+            for j in range(0, 13):
+                self.cards.append(Card.Card(i, j))
